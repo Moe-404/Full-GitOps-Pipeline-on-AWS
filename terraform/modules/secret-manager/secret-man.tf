539fd78c94689442af0afa172afffd72ae7c83ec
Shahd77Fayez <Shahd2003fayez@gmail.com>
2025-06-09 19:45:13 +0300

    Modify Terraform Secret Manager module add Output for getting info of access key

--- terraform/modules/secret-manager/secret-man.tf
+++ terraform/modules/secret-manager/secret-man.tf
@@ -1,5 +1,5 @@
-resource "aws_secretsmanager_secret" "SecMang-Grad" {
-  name = "AWS-GradProject-Secret"
+resource "aws_secretsmanager_secret" "SecMang_Grad" {
+  name = "AWS_GradProject_Secret"
   description = "This secret contains sensitive information for the DB,Redis Credentials."
   
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "ecr_repository_url" {
   value = module.ecr.repository_url
 }
+output "secret_manager_access_key_id" {
+  value = module.secret_manager.access_key_id
+}
+
+output "secret_manager_secret_access_key" {
+  value     = module.secret_manager.secret_access_key
+  sensitive = true
+}
