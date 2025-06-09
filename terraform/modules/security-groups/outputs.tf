b7354dba5ff282c0d96e7f06c4c9167f099f23dc
Dalia-Karam <daliakaram2000@gmail.com>
2025-06-09 22:38:02 +0300

    Feat: Defined the output for the worker nodes' security group id

--- terraform/modules/security-groups/outputs.tf
+++ terraform/modules/security-groups/outputs.tf
@@ -5,3 +5,7 @@ value = aws_security_group.web_sg.id
 output "bastion_sg_id" {
 value = aws_security_group.bastion_sg.id
 }
+
+output "eks_worker_sg_id" {
+  value = aws_security_group.eks_worker_sg.id
+}
